4364db6b0bed59bc34c9f8c58d81ecf1e24b41a7
blibotics <blibotics@gmail.com>
2013-07-02 03:21:08 -0400

    Updating Controller and Routes files to enable the static page functionalities

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,4 +20,28 @@ class PicturesController < ApplicationController
 		]
 	end
 
+	def picture0
+		@picture = {
+			:title	=> "The old church on the coast of White sea",
+			:artist => "Sergey Ershov",
+			:url 		=> "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+		}
+	end
+
+	def picture1
+		@picture = {
+			:title 	=> "Sea Power",
+			:artist => "Stephen Scullion",
+			:url 		=> "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+		}
+	end
+
+	def picture2
+		@picture = {
+			:title 	=> "Into the Poppies",
+			:artist => "John Wihlem",
+			:url 		=> "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+		}
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Photogur::Application.routes.draw do
 #GET 'URL' => 'Controller#Action'
 	get 'pictures' => 'pictures#index'
 
+	get 'pictures/0' => 'pictures#picture0'
+	get 'pictures/1' => 'pictures#picture1'
+	get 'pictures/2' => 'pictures#picture2'
+
 end
 
 	# The priority is based upon order of creation: first created -> highest priority.
