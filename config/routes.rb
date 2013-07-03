3e537fef9fee876532f56fc59b8795a33e6d26a5
blibotics <blibotics@gmail.com>
2013-07-03 18:28:07 -0400

    Refactoring Step #6 to use Form helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Photogur::Application.routes.draw do
 	post 'pictures' => "pictures#create" # this is a new line of code
 
+#Match the HTTP request GET with the URL "/pictures" to the INDEX action in the PicturesController
+#GET 'URL' => 'Controller#Action'
 	get 'pictures' => 'pictures#index'
 
 	get 'pictures/new' => 'pictures#new' # this is also a new line of code
