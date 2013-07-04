df1ad877f8672000527caa1f243823e9c81b0148
blibotics <blibotics@gmail.com>
2013-07-04 13:55:03 -0400

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,18 @@
 class PicturesController < ApplicationController
 
-	def create
-		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-	end
-
 	def index
 		@pictures = Picture.all
-	end
-
-	def new
 	end
 
 	def show
 		@picture = Picture.find(params[:id])
 	end
 
+	def new
+	end
+
+	def create
+		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+	end
 
 end
